e99efb39d95583bf5e1705e3efa45f17290badd8
Florent GUITTRE <florent.guittre@epitech.eu>
2024-05-05 14:16:05 +0200

    fix: remove PropAnim project

--- PropAnim/scripts/class.js
+++
@@ -1,7 +0,0 @@
-class Day {
-    constructor(dayDataArray) {
-        this.date = dayDataArray[1];
-        this.departementName = dayDataArray[3];
-        this.incidenceRate = parseInt(dayDataArray[6]);
-    }
-}
